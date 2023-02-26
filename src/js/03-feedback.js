33d4075df9a709ff5c36149cc365129872e7ecbe
KmiloLopez <108908318+KmiloLopez@users.noreply.github.com>
2023-02-26 16:25:26 -0500

    Update tarea 8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,11 @@
-
+/* import throttle = require("lodash.throttle") */
+import throttle from 'lodash.throttle'
 console.log("conectado")
 
 const validateForm = document.getElementsByClassName('feedback-form')
 
-const submitButton = document.querySelector('button')
-console.log(validateForm)
+/* const submitButton = document.querySelector('button') */
+/* console.log(validateForm) */
 
 validateForm[0].addEventListener("submit", (e)=>{
     e.preventDefault()
@@ -12,11 +13,41 @@ validateForm[0].addEventListener("submit", (e)=>{
     console.log("Email:",email.value)
     console.log("Message:",message.value)
 
+    validateForm[0].email.value= null
+    validateForm[0].message.value= null
+    function functttion(){localStorage.setItem('feedback-form-state', email.value+" "+message.value)}
+    /*  throttle(functttion(),1000); */
+    functttion()
     /* const Play = ({seconds})=>{ */
-    localStorage.setItem('feedback-form-state', email.value+" "+message.value);
+    /* localStorage.setItem('feedback-form-state', email.value+" "+message.value); */
+    
     validateForm[0].reset()
+    
 })
+/* const throttle = (fn, delay)=>{
+    let last 0;
+    return(...args)=>{
+        const now = new Date().getTime();
+        if(now -last <delay){
+            return;
+        }
+        last = now;
+        return fn(...args)
+    }
+} */
+
 validateForm[0].addEventListener('input', (e)=>{
+    e.preventDefault()
+    const {email, message} = e.currentTarget 
+    function functttion(){localStorage.setItem('feedback-form-state', email.value+" "+message.value)}
+   /*  throttle(functttion(),1000); */
+   functttion()
+    /* let inputt = e.target.value
+    console.log(inputt) */
+    /* validateForm[0].email.value= passAndEmail[0] */
+    
+})
+/* validateForm[0].addEventListener('input', (e)=>{
     e.preventDefault()
     const {email, message} = e.currentTarget 
     localStorage.setItem('feedback-form-state', email.value+" "+message.value);
@@ -24,40 +55,14 @@ validateForm[0].addEventListener('input', (e)=>{
 
     let inputt = e.target.value
     console.log(inputt)
-    /* console.log(e.message.value)
-    console.log(e.email.value) */
-    /* spanText.textContent = e.target.value */
-    /*  if (e.target.value === undefined){
-        spanText.textContent ="ANONININI"
-    } */
-    /* if (e.target.value === ""){
-        spanText.textContent ="Anonymous"} */
+    /* validateForm[0].email.value= passAndEmail[0] 
     
-    
-})
+}) */
 
-console.log(localStorage.getItem('feedback-form-state'))
+ console.log(localStorage.getItem('feedback-form-state'))
 let passAndEmail =localStorage.getItem('feedback-form-state').split(" ")
-console.log(passAndEmail)
-console.log("PASSWOERRRRRRRDD:",passAndEmail[0])
+/* console.log(passAndEmail)
+console.log("PASSWOERRRRRRRDD:")  */
 
-/* console.log(validateForm[0].firstChild)
-validateForm[0].firstchild.textContent= passAndEmail[0] */
-let EMAILTEXT = document.querySelector('input')
-console.log("ESTO QUE ES",EMAILTEXT)
-EMAILTEXT.innerHTML=passAndEmail[0]
-EMAILTEXT.innerText=passAndEmail[0]
-EMAILTEXT.textContent=passAndEmail[0]
-EMAILTEXT.setText= passAndEmail[0]
-
-validateForm.innerHTML=passAndEmail[0]
-validateForm.innerText=passAndEmail[0]
-validateForm.textContent=passAndEmail[0]
-validateForm.setText= passAndEmail[0]
-
-
-
-/* let emailtextLocal = validateForm[0].children */
-
- /* validateForm[0].textContent=passAndEmail[0] */
-/* validateForm[1].textContent=passAndEmail[1]  */
+validateForm[0].email.value= passAndEmail[0]
+validateForm[0].message.value= passAndEmail[1]
